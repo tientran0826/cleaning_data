2ef7b29054118df26b203f2daf6cc9b2afb65e14
tientran0826 <tientran0826@gmail.com>
2023-06-20 14:56:48 +0700

    upload files

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import re
 import pandas as pd 
 import numpy as np
 
+def find_max_length(data):
+    max_len = 0
+    for line in data:
+        line = re.sub(r'\([^)]*\)', '', line)[4:].replace(",", "").split()
+        #print(len(line))
+        if len(line) > max_len:
+            max_len = len(line)
+    return max_len
 
 def clean_data(file_path):
     file = open(file_path, "r",encoding="utf-8")
@@ -13,70 +21,57 @@ def clean_data(file_path):
     file.close()
 
     stock_datas = []
+
+    max_len = find_max_length(lines)
+
     for line in lines:
+
         line = re.sub(r'\([^)]*\)', '', line)
         # Get stock name
         stock_name = line[:3]
+
+        # Get stock data
         data = line[4:].replace(",", "").split()
-        if len(data) == 9:
+
+        # If a line is missing columns
+        if len(data) < max_len:
+            # Fill column 9 with 0 value
             if data[-1].endswith("%"):
-                data.insert(8, 0)
+                data.insert(len(data)-1, 0)
             else:
-                data.insert(9, "0%")
+            # Fill column 10 with "0%"
+                data.insert(len(data), "0%")
+
+        # Remove "%" in the last column
         data[-1] = str(data[-1])[:-1]
 
-        stock_data = {
-            "Co phieu" : stock_name,
-            "Ngay": data[0],
-            "KL giao dich rong": float(data[1]),
-            "Gia tri giao dich rong": float(data[2]),
-            "Thay doi": float(data[3]),
-            "Khoi luong mua": float(data[4]),
-            "Gia tri mua": float(data[5]),
-            "Khoi luong ban": float(data[6]),
-            "Gia tri ban": float(data[7]),
-            "Room con lai": float(data[8]),
-            "Dang so huu": float(data[9])
-        }
-        stock_datas.append(stock_data)
+        # Add Stock Name to index 0
+        data.insert(0,stock_name)
+        stock_datas.append(data)
 
     output = pd.DataFrame(stock_datas)
-    #output.to_csv("output.csv",index=False)
-    output['Ngay'] = pd.to_datetime(output['Ngay'], format='%d/%m/%Y')
+    output.iloc[:,1] = pd.to_datetime(output.iloc[:,1], format='%d/%m/%Y')
 
     grouped_dataframes = {}
-    for group_name, group_indices in output.groupby('Co phieu').groups.items():
+    for group_name, group_indices in output.groupby(by = 0).groups.items():
         # Get the group DataFrame
         group_df = output.loc[group_indices]
-        # Store the group DataFrame in the dictionary
         
-        reversed_df = group_df.sort_values('Ngay')
+        reversed_df = group_df.sort_values(by = 1)
 
-        buy_volume = reversed_df['Khoi luong mua'].cumsum()
-        buy_value = reversed_df['Gia tri mua'].cumsum()
-
-        sell_volume = reversed_df['Khoi luong ban'].cumsum()
-        sell_value = reversed_df['Gia tri ban'].cumsum()
-
-        net_volume = reversed_df['KL giao dich rong'].cumsum()
-        net_value = reversed_df['Gia tri giao dich rong'].cumsum()
-
-        group_df['Khoi luong mua'] = buy_volume[::-1]
-        group_df['Gia tri mua'] = buy_value[::-1]
-
-        group_df['Khoi luong ban'] = sell_volume[::-1]
-        group_df['Gia tri ban'] = sell_value[::-1]
-
-        group_df['Room con lai'] = group_df['Room con lai'].replace(0, np.nan).fillna(method='backfill')
-        group_df['Dang so huu'] = group_df['Dang so huu'].replace(0, np.nan).fillna(method='backfill')
-
-        group_df['KL giao dich rong'] = net_volume[::-1]
-        group_df['Gia tri giao dich rong'] = net_value[::-1]
-
+        for i in range(2,9):
+            if i == 4:
+                continue
+            
+            cumsum_value = reversed_df.iloc[:,i].astype('float64').cumsum()
+            group_df.iloc[:,i] = cumsum_value[::-1]
+        
+        group_df.iloc[:,-1] = group_df.iloc[:,-1].astype('float64').replace(0, np.nan).fillna(method='backfill')
+        group_df.iloc[:,-2] = group_df.iloc[:,-2].astype('float64').replace(0, np.nan).fillna(method='backfill')
         grouped_dataframes[group_name] = group_df
 
     for key,value in grouped_dataframes.items():
-        pd.DataFrame(value).set_index('Ngay').to_csv(f'{key}.csv')
+        pd.DataFrame(value).to_csv(f'{key}.csv',index=False)
 
 file_path = "data17.txt"
 clean_data(file_path=file_path)
